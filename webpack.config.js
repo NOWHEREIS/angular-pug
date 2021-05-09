d22dec31efc1f8beac63767e341e051185a1fb23
Simón Espinosa <saespinosac@gmail.com>
2021-05-08 21:31:50 -0400

    add pug

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const {AngularWebpackPlugin} = require('@ngtools/webpack/src/ivy/plugin');
+const {SourceMapDevToolPlugin} = require('webpack');
+
+module.exports = (config) => {
+  // add rule to convert pug into html files
+  config.module.rules.push({
+    test: /\.pug$/,
+    use: [
+      {loader: 'apply-loader'},
+      {loader: 'pug-loader'},
+    ],
+  })
+
+  const indexForAngularWebpackPlugin = config.plugins?.findIndex((plugin) => {
+    return plugin instanceof AngularWebpackPlugin;
+  });
+  if (indexForAngularWebpackPlugin > -1) {
+    const oldOptions = config.plugins?.[indexForAngularWebpackPlugin].pluginOptions;
+    oldOptions.directTemplateLoading = false;
+    config.plugins?.splice(indexForAngularWebpackPlugin);
+    config.plugins?.push(new AngularWebpackPlugin(oldOptions));
+  }
+  const configPluginSourceMapDevToolPlugin = {
+    filename: "[file].map",
+    include: [
+      /js$/, /css$/
+    ],
+    sourceRoot: "webpack:///",
+    moduleFilenameTemplate: "[resource-path]"
+  }
+
+  if (config.entry?.main?.includes('test')) {
+    const indexForSourceMapDevToolPlugin = config.plugins?.findIndex(plugin => {
+      return plugin instanceof SourceMapDevToolPlugin;
+    })
+
+    if (indexForSourceMapDevToolPlugin > -1) {
+      config.plugins?.splice(indexForSourceMapDevToolPlugin);
+    } else {
+      config.plugins?.push(new SourceMapDevToolPlugin(configPluginSourceMapDevToolPlugin));
+    }
+
+  }
+
+  return config;
+}
